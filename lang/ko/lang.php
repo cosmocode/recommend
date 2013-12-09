60365c34592db37447f22f163f0ed083553e6fda
Myeongjin <aranet100@gmail.com>
2013-12-09 16:23:18 +0900

    Add Korean translation

---
+++ lang/ko/lang.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['allowanonymous'] = '익명 사용자가 문서를 추천할 수 있습니다. captcha helper 플러그인을 사용할 수 있을 때에만 설정해야 합니다.';
